a04e45db678b74bcb42fcbbe33ac6c574963d932
orionheidi <hajdidjukic@gmail.com>
2019-04-05 13:28:30 +0200

    2 - car controller & actions

--- app/Car.php
+++ app/Car.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-    //
+    protected $fillable = [
+        'brand','model','year','maxSpeed','isAutomatic','engine','numberOfDoor'
+    ];
 }
