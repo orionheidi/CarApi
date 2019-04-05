3f31227a6eae5fb7693664addf58c453a2514138
orionheidi <hajdidjukic@gmail.com>
2019-04-05 13:54:17 +0200

    3 - car validation

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -7,79 +7,54 @@ use App\Car;
 
 class CarController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Car::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
+        $this->validate($request,
+        [
+            'brand' => 'required|min:2',
+            'model' => 'required|min:2',
+            'year' => 'required',
+            'maxSpeed' => 'integer|between:20,300',
+            'isAutomatic' => 'required',
+            'engine' => 'required',
+            'numberOfDoor' => 'required|integer|between:2,5'
+        ]);
+
         \Log::info(print_r($request->all(),true));
         return Car::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return Car::findOrFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Car $car)
-    {
+    {    
+        $this->validate($request,
+        [
+            'brand' => 'required|min:2',
+            'model' => 'required|min:2',
+            'year' => 'required',
+            'maxSpeed' => 'integer|between:20,300',
+            'isAutomatic' => 'required',
+            'engine' => 'required',
+            'numberOfDoor' => 'required|integer|between:2,5'
+        ]);
         $car->update($request->all());
         return $car;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         return Car::destroy($id);
